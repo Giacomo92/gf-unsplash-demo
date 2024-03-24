5711f1b4f83da010a4a5f543d757903fce17a76f
Giacomo92 <info@giacomofabbian.it>
2024-03-24 20:38:41 +0100

    feat: add ImageWithSkeleton

---
+++ src/components/ImageWithSkeleton.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+
+const ImageWithSkeleton = ({ src, alt, srcSet, sizes }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  return (
+    <div style={{ position: 'relative', width: '100%', height: 'auto' }}>
+      {!isLoaded && (
+        <div
+          className="skeleton"
+          style={{
+          backgroundColor: '#ccc',
+          width: '100%',
+          paddingTop: '56.25%',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+        }}>
+        </div>
+      )}
+      <img
+        className="mb-0 w-100 h-auto"
+        src={src}
+        alt={alt}
+        srcSet={srcSet}
+        sizes={sizes}
+        style={{ width: '100%', height: 'auto', display: isLoaded ? 'block' : 'none' }}
+        onLoad={() => setIsLoaded(true)}
+      />
+    </div>
+  );
+};
+
+export default ImageWithSkeleton;
