db9c8e66046e03bfc7b19417370d2944595bb3bc
Giacomo92 <info@giacomofabbian.it>
2024-03-24 20:39:11 +0100

    feat: add new page immagini-preferite

---
+++ src/pages/immagini-preferite.js
@@ -0,0 +1,33 @@
+import * as React from "react"
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as styles from "../components/index.module.css"
+import FavoriteImages from "../components/FavoriteImages";
+
+const IndexPage = () => (
+  <Layout>
+    <Row>
+      <Col>
+        <div className={styles.textCenter}>
+          <h1 className='m-20'>
+           Le mie immagini preferite
+          </h1>
+        </div>
+        <FavoriteImages/>
+      </Col>
+    </Row>
+  </Layout>
+)
+
+/**
+ * Head export to define metadata for the page
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
+ */
+export const Head = () => <Seo title="Home" />
+
+export default IndexPage
